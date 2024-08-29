55ff8cec5c4a41cb9a639fd456daae90798d6ff6
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-08-28 21:08:30 -0500

    added user and journal models

---
+++ backend/src/models/Journal.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export type JournalType = {
+    _id: string;
+    userId: string;
+    title: string;
+    journalType: "task" | "event" | "note" | "idea" | "mood" | "goal" | "habit" | "gratitude" | "quote" | "memory" | "reflection";
+    entry: string;
+}
+
+const journalSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    title: { type: String, required: true },
+    journalType: {
+        type: String,
+        enum: ["task", "event", "note", "idea", "mood", "goal", "habit", "gratitude", "quote", "memory", "reflection"],
+        required: true
+    },
+    entry: { type: String, required: true }
+})
+
+const Journal = mongoose.model<JournalType>("Journal", journalSchema)
+
+export default Journal
